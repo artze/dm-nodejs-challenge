cbc4afed031afb4aa012043eab8a6e7674ce3c01
artze <arthurwtm@gmail.com>
2018-12-02 20:41:52 +0100

    Create validatePayment

--- application/lib/validator/validateUser.js
+++ application/lib/validator/validateUser.js
@@ -7,34 +7,34 @@ module.exports = function validateUser(user) {
      * validate id
      */
     if (!inputDataValidator.validateId(user.id)) {
-        throw new InvalidInputError('\'id\' field is missing or has incorrect format');
+        throw new InvalidInputError('ID field in User is missing or has incorrect format');
     }
 
     /**
      * validate userName
      */
     if (!inputDataValidator.validateUserName(user.userName)) {
-        throw new InvalidInputError('\'userName\' field is missing or has incorrect format');
+        throw new InvalidInputError('USER_NAME field in User is missing or has incorrect format');
     }
 
     /**
      * validate first name
      */
     if (user.firstName && !inputDataValidator.validateFirstOrLastName(user.firstName)) {
-        throw new InvalidInputError('\'firstName\' has incorrect format')
+        throw new InvalidInputError('FIRST_NAME in User has incorrect format')
     }
 
     /**
      * validate last name
      */
     if (user.lastName && !inputDataValidator.validateFirstOrLastName(user.lastName)) {
-        throw new InvalidInputError('\'lastName\' has incorrect format');
+        throw new InvalidInputError('LAST_NAME in User has incorrect format');
     }
 
     /**
      * validate email according to RFC
      */
     if (user.email && !inputDataValidator.validateEmail(user.email)) {
-        throw new InvalidInputError('\'email\' has incorrect format');
+        throw new InvalidInputError('EMAIL in User has incorrect format');
     }
 }
