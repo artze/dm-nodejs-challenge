d428afa4cf05050923b5abe1b6792e0c2bb00bf4
artze <arthurwtm@gmail.com>
2018-12-02 16:36:10 +0100

    First validateUser implementation

---
+++ application/lib/validateInput.js
@@ -0,0 +1,61 @@
+const emailValidator = require('email-validator')
+
+function validateId(id) {
+    return /^[a-zA-Z0-9]{64}$/.test(id);
+}
+
+function validateUserName(userName) {
+    return /^[a-zA-Z0-9_.]{1,20}$/.test(userName);
+}
+
+function validateFirstOrLastName(firstOrLastName) {
+    return /^[a-zA-Z]{1,100}$/.test(firstOrLastName);
+}
+
+function validateUser(user) {
+
+    /**
+     * validate id
+     */
+    if (!validateId(user.id)) {
+        console.error('id format error');
+    }
+
+    /**
+     * validate userName
+     */
+    if (!validateUserName(user.userName)) {
+        console.error('user name format error');
+    }
+
+    /**
+     * validate first name
+     */
+    if (user.firstName && !validateFirstOrLastName(user.firstName)) {
+        console.error('first name error')
+    }
+
+    /**
+     * validate last name
+     */
+    if (user.lastName && !validateFirstOrLastName(user.lastName)) {
+        console.error('last name error');
+    }
+
+    /**
+     * validate email according to RFC
+     */
+    if (user.email && !emailValidator.validate(user.email)) {
+        console.error('email error');
+    }
+}
+
+var user = {
+    id: 'fyPZYzsBthguAgbMZkAAukWQWASmtOyPwVLw11fQATpxcOgpgUZyZAiyjHWimIcG',
+    userName: 'Arturo_Tremblay',
+    firstName: 'Charlotte',
+    lastName: 'Bradtke',
+}
+
+validateUser(user)
+console.log('done')
