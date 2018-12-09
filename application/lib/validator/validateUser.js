fca37596c63495c57c68b7cf92879afef68d7f16
artze <arthurwtm@gmail.com>
2018-12-09 17:21:51 +0100

    Add required field presence validation to validateMerchant

--- application/lib/validator/validateUser.js
+++ application/lib/validator/validateUser.js
@@ -11,35 +11,35 @@ module.exports = function validateUser(user) {
   }
 
   /**
-   * validate id
+   * validate id format
    */
   if (user.id && !inputDataValidator.validateId(user.id)) {
     throw new InvalidInputError('ID field in User is missing or has incorrect format');
   }
 
   /**
-   * validate userName
+   * validate userName format
    */
   if (user.userName && !inputDataValidator.validateUserName(user.userName)) {
     throw new InvalidInputError('USER_NAME field in User is missing or has incorrect format');
   }
 
   /**
-   * validate first name
+   * validate first name format
    */
   if (user.firstName && !inputDataValidator.validateFirstOrLastName(user.firstName)) {
     throw new InvalidInputError('FIRST_NAME in User has incorrect format')
   }
 
   /**
-   * validate last name
+   * validate last name format
    */
   if (user.lastName && !inputDataValidator.validateFirstOrLastName(user.lastName)) {
     throw new InvalidInputError('LAST_NAME in User has incorrect format');
   }
 
   /**
-   * validate email according to RFC
+   * validate email according to RFC format
    */
   if (user.email && !inputDataValidator.validateEmail(user.email)) {
     throw new InvalidInputError('EMAIL in User has incorrect format');
