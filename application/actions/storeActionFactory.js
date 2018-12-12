4259bf4ff8adc95dc6301405951191a96dda8520
artze <arthurwtm@gmail.com>
2018-12-12 18:58:52 +0100

    Create input objects based on type

--- application/actions/storeActionFactory.js
+++ application/actions/storeActionFactory.js
@@ -1,4 +1,8 @@
-const validator = require('../lib/validator');
+const validator = require('../lib/validator/validator');
+const InvalidInputError = require('../lib/validator/InvalidInputError');
+const User = require('../models/User');
+const Payment = require('../models/Payment');
+const Merchant = require('../models/Merchant');
 
 /**
  * Create storeAction with dependencies
@@ -20,12 +24,33 @@ module.exports = function storeActionFactory(p2pFetch, hostedFetch) {
   async function storeAction(inputData) {
     // Your code should be here:
     // 1. Validate inputData
-    try {
-      validator(inputData)
-    } catch (error) {
-      console.error(error);
-      return;
-    }
+
+    // try {
+    //   validator(inputData)
+    // } catch (error) {
+    //   console.error(error);
+    //   return;
+    // }
+
+    let objectArr = [];
+    inputData.forEach(function(inputObject, index) {
+      if (!validator.validateType(inputObject.type)) {
+        throw new InvalidInputError('Input Error')
+      }
+      if (inputObject.type === 'user') {
+        const user = new User(inputObject.id, inputObject.type, inputObject.userName, inputObject.firstName, inputObject.lastName, inputObject.email);
+        objectArr.push(user)
+      } else if (inputObject.type === 'payment') {
+        const payment = new Payment(inputObject.id, inputObject.type, inputObject.fromUserId, inputObject.toMerchantId, inputObject.toUserId, inputObject.amount, inputObject.createdAt);
+        objectArr.push(payment);
+      } else if (inputObject.type === 'merchant') {
+        const merchant = new Merchant(inputObject.id, inputObject.type, inputObject.name);
+        objectArr.push(merchant);
+      }
+    })
+
+    console.log(objectArr);
+
     // 2. Store inputData in external service(s)
     await hostedFetch('http://localhost:3000/api/app-data', {
       method: 'POST',
